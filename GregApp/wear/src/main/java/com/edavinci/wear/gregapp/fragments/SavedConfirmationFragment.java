370da048763577830084b3c6ef9600c29e78e4aa
dancewicz.grzegorz <dancewicz.grzegorz@gmail.com>
2016-10-06 22:05:01 +0200

    added adding of itmes once finished

--- GregApp/wear/src/main/java/com/edavinci/wear/gregapp/fragments/SavedConfirmationFragment.java
+++ GregApp/wear/src/main/java/com/edavinci/wear/gregapp/fragments/SavedConfirmationFragment.java
@@ -9,6 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.edavinci.wear.gregapp.R;
+import com.edavinci.wear.gregapp.activities.ReminderDTO;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.edavinci.wear.gregapp.activities.MainActivity.REMINDERS_LIST_FILENAME;
 
 /**
  * Created by Grzegorz on 2016-09-30.
@@ -17,7 +28,7 @@ import com.edavinci.wear.gregapp.R;
 public class SavedConfirmationFragment extends Fragment{
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
             //super.onCreateContentView(inflater, container, savedInstanceState);
             View result = inflater.inflate(R.layout.save_reminder, container, false);
             result.findViewById(R.id.saveButton).setOnClickListener(new View.OnClickListener() {
@@ -27,9 +38,17 @@ public class SavedConfirmationFragment extends Fragment{
                     intent.putExtra(ConfirmationActivity.EXTRA_ANIMATION_TYPE,
                             ConfirmationActivity.SUCCESS_ANIMATION);
                     intent.putExtra(ConfirmationActivity.EXTRA_MESSAGE,
-                            "GOTOWE");
-                    //TODO copy working item as new item and save the list
+                            R.string.reminder_created);
+
+                    List<ReminderDTO> savedReminders = getSavedReminders(container);
+                    savedReminders.add(savedReminders.get(0));
+                    saveReminders(savedReminders,container);
                     startActivity(intent);
+
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                    }
                     getActivity().onBackPressed();
                 }
             });
@@ -39,4 +58,60 @@ public class SavedConfirmationFragment extends Fragment{
 
         }
 
+    public static List<ReminderDTO> getSavedReminders(ViewGroup container) {
+        ObjectInputStream oin = null;
+        ArrayList<ReminderDTO> savedReminders = new ArrayList<ReminderDTO>();
+        try {
+            FileInputStream fin = null;
+            try {
+                fin = container.getContext().openFileInput(REMINDERS_LIST_FILENAME);
+                oin = new ObjectInputStream(fin);
+                if (oin != null) {
+                    savedReminders = (ArrayList<ReminderDTO>) oin.readObject();
+                }
+            } catch (Exception e) {
+                //
+            }
+
+            if (savedReminders.size() == 0) {
+                savedReminders.add(0, new ReminderDTO());
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            try {
+                oin.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return savedReminders;
+    }
+
+    public static void saveReminders(List<ReminderDTO> reminders, ViewGroup container) {
+        ObjectOutputStream oout = null;
+        try {
+            FileInputStream fin = null;
+            try {
+                FileOutputStream fout = container.getContext().openFileOutput(REMINDERS_LIST_FILENAME, container.getContext().MODE_PRIVATE);
+                oout = new ObjectOutputStream(fout);
+                oout.writeObject(reminders);
+            } catch (Exception e) {
+                //
+            }
+            if (reminders.size() == 0) {
+                reminders.add(0, new ReminderDTO());
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            try {
+                oout.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
